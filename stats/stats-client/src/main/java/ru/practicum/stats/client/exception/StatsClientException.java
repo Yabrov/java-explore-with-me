66c0b2a7b4fed4095d6f061b13ec0443b9d6866a
yabro <yabrow@yandex.ru>
2023-04-16 02:22:02 +0500

    feat: completed stats client

---
+++ stats/stats-client/src/main/java/ru/practicum/stats/client/exception/StatsClientException.java
@@ -0,0 +1,8 @@
+package ru.practicum.stats.client.exception;
+
+public class StatsClientException extends RuntimeException {
+
+    public StatsClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
