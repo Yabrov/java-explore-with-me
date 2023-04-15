ec942ad000f15c89085ed05b5b262f8e81797fb2
yabro <yabrow@yandex.ru>
2023-04-16 02:23:04 +0500

    fix: fixed DTOs

--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,10 +1,13 @@
 package ru.practicum.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,24 +15,18 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@AllArgsConstructor(onConstructor = @__(@JsonCreator))
 public class EndpointHitDto {
 
     private String app;
-    private String uri;
-    private String ip;
-    private LocalDateTime timestamp;
 
-    public EndpointHitDto(
-            String app,
-            String uri,
-            String ip,
-            @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-            @JsonSerialize(using = LocalDateTimeSerializer.class)
-            @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-            LocalDateTime timestamp) {
-        this.app = app;
-        this.uri = uri;
-        this.ip = ip;
-        this.timestamp = timestamp;
-    }
+    private String uri;
+
+    private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime timestamp;
 }
--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
@@ -1,14 +1,20 @@
 package ru.practicum.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor(onConstructor = @__(@JsonCreator))
 public class ViewStatsDto {
+
     private String app;
+
     private String uri;
+
     private Integer hits;
 }
