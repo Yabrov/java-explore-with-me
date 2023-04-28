bf8ddc2e18b825abb2608d718585b021bda1fd5c
TOP_GUN <yabrow@yandex.ru>
2023-04-28 17:04:15 +0500

    feat: added aspect

---
+++ main-server/src/main/java/ru/practicum/main/server/aspect/StatsServerAspect.java
@@ -0,0 +1,44 @@
+package ru.practicum.main.server.aspect;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import ru.practicum.stats.client.service.StatsServerClient;
+import ru.practicum.stats.dto.EndpointHitDto;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class StatsServerAspect {
+
+    private final StatsServerClient statsServerClient;
+
+    @Pointcut("execution(public * ru.practicum.main.server.controller.pub.EventPublicController.getEvent())")
+    public void statsCollectingTrigger() {
+    }
+
+    @Before("statsCollectingTrigger()")
+    public void aspect(JoinPoint jp) {
+        try {
+            HttpServletRequest request = (HttpServletRequest) jp.getArgs()[1];
+            EndpointHitDto endpointHitDto = new EndpointHitDto(
+                    "ewm-service",
+                    request.getRequestURI(),
+                    request.getRemoteAddr(),
+                    LocalDateTime.now()
+            );
+            EndpointHitDto response = statsServerClient.sendEndpointHit(endpointHitDto);
+            log.info("Saved stats: {}", response.toString());
+        } catch (Exception e) {
+            log.error("Error when sending stats", e);
+        }
+    }
+}
